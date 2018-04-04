94931011ef8ad50d2a85d1f6ddd0a493c7c9314c
Joseph Parker <jparker010190@gmail.com>
2018-04-04 23:46:32 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,4 +1,11 @@
-CREATE TABLE characters (
-  id INTERGER PRIMARY KEY, 
-  series 
+CREATE TABLE series (
+  id integer PRIMARY KEY,
+  title text,
+  author_id integer,
+  subgenre_id integer
+);
+
+CREATE TABLE subgenres(
+name text,
+id integer primary key
 );
