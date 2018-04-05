1714ab7841cfd66ad03195069c63de3e40545671
Joseph Parker <jparker010190@gmail.com>
2018-04-05 00:45:12 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -8,7 +8,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "select species, count(species) from characters group by species having count(species) = max(species);"
+  "select species, count(species) from characters where species = 'human'"
 end
 
 def select_name_and_series_subgenres_of_authors
