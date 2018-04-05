b55223426e405166a46e495125df575c7fe7ff76
Joseph Parker <jparker010190@gmail.com>
2018-04-05 00:49:18 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -20,5 +20,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "select characters.name, count(books.title) from characters INNER JOIN series on characters.series_id = series.id INNER JOIN books ON books.series_id = series.id order by name, title desc;"
+  "select characters.name, count(books.title) from characters INNER JOIN series on characters.series_id = series.id INNER JOIN books ON books.series_id = series.id order by name, books.title desc;"
 end
