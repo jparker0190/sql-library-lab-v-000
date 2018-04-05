4b7e50a40c63c41623f25eb09ba873c5e03348ea
Joseph Parker <jparker010190@gmail.com>
2018-04-05 00:15:14 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -12,7 +12,7 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "select authors.name, subgenres.name from authors, subgenres where authors.id = subgenres.id"
 end
 
 def select_series_title_with_most_human_characters
